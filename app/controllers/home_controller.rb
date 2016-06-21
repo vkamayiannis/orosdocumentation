555ae2ab716d8cef56eaa751158a3ee3b079b133
Valantis Kamayiannis <kamayiannis@yahoo.com>
2016-06-21 21:23:23 +0300

    Initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,27 @@
+class HomeController < ApplicationController
+  def index
+    @libraries = get_libraries
+  end
+
+  protected
+  def get_libraries
+    libraries = []
+    libraries << {
+      name: "AlumilERP.dll",
+      customer: "Alumil Greece",
+      url: "http://www.google.com"
+    }
+
+    libraries << {
+      name: "DCManage",
+      customer: "Orosimo software",
+      url: "http://www.in.gr"
+    }
+
+    libraries << {
+      name: "Elgeka.dll",
+      customer: "Elgeka - Ferfelis Romania",
+      url: "http://www.elgeka.com"
+    }
+  end
+end
