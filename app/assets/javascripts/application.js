65d5d32781405ba871fb5a43524b262ceca54b11
Valantis Kamayiannis <kamayiannis@yahoo.com>
2016-06-21 23:00:32 +0300

    fixed search box - changed layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,68 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
+
+$(function() {
+	var search = $('.project-search'),
+		content = $('.projects-list'),
+		matches = $(), index = 0;
+
+	// Listen for the text input event
+	search.on('input', function(e) {
+
+		// Only search for strings 2 characters or more
+		if (search.val().length >= 2) {
+			// Use the highlight plugin
+			content.highlight(search.val(), function(found) {                
+                found.parent().parent().css('background','yellow');
+			});
+		}
+		else {
+			content.highlightRestore();
+		}
+	});
+});
+
+(function($) {
+	var termPattern;
+	$.fn.highlight = function(term, callback) {
+		return this.each(function() {
+			var elem = $(this);
+			if (!elem.data('highlight-original')) {
+				// Save the original element content
+				elem.data('highlight-original', elem.html());
+			} else {
+				// restore the original content
+				elem.highlightRestore();
+			}
+			termPattern = new RegExp('(' + term + ')', 'ig');
+			// Search the element's contents
+			walk(elem);
+			// Trigger the callback
+			callback && callback(elem.find('.match'));
+		});
+	};
+	$.fn.highlightRestore = function() {
+		
+		return this.each(function() {
+			var elem = $(this);
+			elem.html(elem.data('highlight-original'));
+		});
+		
+	};
+
+	function walk(elem) {
+		elem.contents().each(function() {
+			if (this.nodeType == 3) { // text node
+				if (termPattern.test(this.nodeValue)) {
+					$(this).replaceWith(this.nodeValue.replace(termPattern, '<span class="match">$1</span>'));
+				}
+			} else {
+				walk($(this));
+			}
+		});
+	}
+
+})(jQuery); 
