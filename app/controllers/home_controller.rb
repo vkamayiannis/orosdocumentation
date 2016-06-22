1715454649caf8e0ebfa0cf6e80c654b8ae88a55
Valantis Kamayiannis <v.kamayiannis@orosimosoftware.gr>
2016-06-22 14:02:07 +0300

    added more projects in list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,7 +9,7 @@ class HomeController < ApplicationController
     libraries << {
       name: "AlumilERP.dll",
       customer: "Alumil Greece",
-      url: ""
+      url: "/docs/AlumilERP/html/Alumil ERP DLL.htm"
     }
 
     libraries << {
@@ -21,6 +21,36 @@ class HomeController < ApplicationController
     libraries << {
       name: "Elgeka.dll",
       customer: "Elgeka - Ferfelis Romania",
+      url: ""
+    }
+
+    libraries << {
+      name: "ImportSfa.dll",
+      customer: "Elgeka - Ferfelis Romania",
+      url: ""
+    }
+
+    libraries << {
+      name: "Makios.dll",
+      customer: "Makios S.A.",
+      url: ""
+    }
+
+    libraries << {
+      name: "MakiosBulgConnection.dll",
+      customer: "Makios S.A.",
+      url: ""
+    }
+
+    libraries << {
+      name: "MakiosBulg.dll",
+      customer: "Makios S.A.",
+      url: ""
+    }
+
+    libraries << {
+      name: "MakiosFiles.dll",
+      customer: "Makios S.A.",
       url: ""
     }
   end
